e4e0d470b64aec771db4d46c013ec97ca1847afd
Janhavi <patil.janhavi1207@gmail.com>
2022-10-24 18:08:26 +0530

    added stats component

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -3,7 +3,7 @@ import styles from '../style'
 import { arrowUp } from '../assets'
 
 const GetStarted = () => (
-    <div className={`${styles.flexCenter} w-[120px] h-[120px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+    <div className={`${styles.flexCenter} hover:animate-bounce w-[120px] h-[120px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
       <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
         <div className={`${styles.flexStart} flex-row`}>
           <p className='font-poppins font-md leading-[23px]'><span className='text-gradient mr-2'>Get</span>
