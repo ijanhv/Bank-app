d6be9761ffe2499e43cccd3b665e595bdf6a0469
Janhavi <patil.janhavi1207@gmail.com>
2022-10-24 10:07:35 +0530

    initial setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { close, logo, menu } from '../assets';
+import { navLinks } from '../constants';
+
+const Navbar = () => {
+  return (
+    <nav className='w-full'>
+
+    </nav>
+  )
+}
+
+export default Navbar
